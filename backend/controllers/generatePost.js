10c77360d2aa56041fb33b0c68b5550ec5287520
bhavesh1432 <118457534+bhavesh-rawal@users.noreply.github.com>
2024-03-01 11:21:30 +0530

    Node-cron add, Facebook, Instagram and Twitter posting Complete

---
+++ backend/controllers/generatePost.js
@@ -0,0 +1,190 @@
+const axios = require("axios");
+require("dotenv").config();
+const Replicate = require("replicate");
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} = require("@google/generative-ai");
+const { database } = require("../firebase");
+const fs = require("fs");
+const { twitterClient } = require("../twitterClient");
+
+const getfirebaseDatabase = async () => {
+    console.log("enter getfirebaseDatabase");
+    return new Promise((resolve, reject) => {
+        const ref = database.ref("/facebook/pageDeatail");
+        ref.once(
+            "value",
+            (snapshot) => {
+                resolve(snapshot.val());
+            },
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+};
+const postCaptionsGenerate = async (data) => {
+    console.log("enter postCaptionsGenerate");
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY_GEMINI);
+    const model = genAI.getGenerativeModel({ model: process.env.MODEL_NAME });
+    const generationConfig = {
+        temperature: 1,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+    ];
+
+    const chat = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [],
+    });
+    const result = await chat.sendMessage(
+        `${data.prompt} ${process.env.TEXT_PROMPT}`
+    );
+    const response = result.response;
+    return response.text();
+};
+const postImageGenerate = async (data) => {
+    console.log("enter postImageGenerate");
+
+    const replicate = new Replicate({
+        auth: process.env.REPLICATE_API_TOKEN,
+    });
+    const output = await replicate.run(process.env.REPLICATE_MODEL_NAME, {
+        input: {
+            width: 1024,
+            height: 1024,
+            prompt: data.prompt,
+            scheduler: "K_EULER",
+            num_outputs: 1,
+            guidance_scale: 0,
+            negative_prompt: "worst quality, low quality",
+            num_inference_steps: 4,
+        },
+    });
+    return output[0];
+};
+const postImageFacebook = async (data) => {
+    try {
+        console.log("enter postImageFacebook");
+        const formData = new FormData();
+        formData.append("message", data.caption);
+        formData.append("access_token", data.access_token);
+        formData.append("url", data.img);
+
+        const response = await axios.post(
+            `${process.env.FACEBOOK_ENDPOINT}${data.PageID}/photos`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("error", error);
+        throw error;
+    }
+};
+const postImageInstagram = async (data) => {
+    try {
+        console.log("enter postImageInstagram");
+
+        const instagramBusinessAccountID = await axios.get(
+            `${process.env.FACEBOOK_ENDPOINT}${data.PageID}?fields=instagram_business_account&access_token=${data.access_token}`
+        );
+
+        const responeseCreationID = await axios.post(
+            `${process.env.FACEBOOK_ENDPOINT}${instagramBusinessAccountID.data.instagram_business_account.id}/media?`,
+            {
+                image_url: data.img,
+                caption: data.caption,
+                access_token: data.access_token,
+            }
+        );
+
+        const Result = await axios.post(
+            `${process.env.FACEBOOK_ENDPOINT}${instagramBusinessAccountID.data.instagram_business_account.id}/media_publish?`,
+            {
+                creation_id: responeseCreationID.data.id,
+                access_token: data.access_token,
+            }
+        );
+
+        return Result.data;
+    } catch (error) {
+        console.error("error", error);
+        throw error;
+    }
+};
+const postImageTwitter = async (data) => {
+    const uri = data.img;
+    const filename = "image.png";
+    console.log("enter postImageTwitter");
+    downloadImage(uri, filename, async () => {
+        try {
+            const mediaId = await twitterClient.v1.uploadMedia("./image.png");
+            const mediaPublish = await twitterClient.v2.tweet({
+                text: data.caption,
+                media: {
+                    media_ids: [mediaId],
+                },
+            });
+            deletePhoto("./image.png");
+            return mediaPublish.data;
+        } catch (error) {
+            console.error("Error uploading media to Twitter:", error);
+            throw error;
+        }
+    });
+};
+const downloadImage = async (imageUrl, localFilePath, callback) => {
+    const response = await axios({
+        url: imageUrl,
+        method: "GET",
+        responseType: "stream",
+    });
+    await response.data
+        .pipe(fs.createWriteStream(localFilePath))
+        .on("close", callback);
+};
+const deletePhoto = (filePath) => {
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error("Error deleting photo:", err);
+            return;
+        }
+    });
+};
+
+module.exports = {
+    postImageInstagram,
+    postCaptionsGenerate,
+    getfirebaseDatabase,
+    postImageFacebook,
+    postImageGenerate,
+    postImageTwitter,
+};
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+var cron = require('node-cron');
+const {
+    postImageFacebook,
+    getfirebaseDatabase,
+    postImageGenerate,
+    postCaptionsGenerate,
+    postImageInstagram,
+    postImageTwitter } = require('./controllers/generatePost');
+
+let i = 1
+cron.schedule('0 7,11,15,19', async () => {
+    console.log('running a task count', i++);
+    await mainFunction()
+
+});
+
+const mainFunction = async () => {
+    try {
+        const pageData = await getfirebaseDatabase();
+        const responseImage = await postImageGenerate(pageData);
+        const responseCaption = await postCaptionsGenerate(pageData);
+        const originalCaption = responseCaption.replace(/[^\w\s]/gi, "");
+        const data = { ...pageData, caption: originalCaption, img: responseImage }
+        await postImageTwitter(data);
+        await postImageFacebook(data)
+        await postImageInstagram(data)
+        console.log('Done ✅')
+    } catch (error) {
+        console.log(error)
+    }
+}
+//    0 7,11,15,19 * * *    => 7am, 11pm, 3pm, 7pm
+//   */5 * * * * *  => 5 sec
+//   * * * * *  => every minutes
