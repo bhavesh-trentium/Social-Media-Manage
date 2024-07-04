ccc549b3f2aad29ac72f2d74f77a34ebb76ffd5c
bhavesh-trentium <bhaveshr@trentiums.net>
2024-07-04 16:31:39 +0530

    formating Update

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,9 +2,9 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://social-media-manage-3a16d-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://social-media-manage-3a16d-default-rtdb.firebaseio.com/",
 });
 
 const database = admin.database();
-module.exports = { database }
+module.exports = { database };
--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,36 @@
-var cron = require('node-cron');
+var cron = require("node-cron");
 const {
-    postImageFacebook,
-    getfirebaseDatabase,
-    postImageGenerate,
-    postCaptionsGenerate,
-    postImageInstagram,
-    postImageTwitter,
-    syncTwiiterToken } = require('./controllers/generatePost');
+  postImageFacebook,
+  getfirebaseDatabase,
+  postImageGenerate,
+  postCaptionsGenerate,
+  postImageInstagram,
+  postImageTwitter,
+  syncTwiiterToken,
+} = require("./controllers/generatePost");
 
-let i = 1
-cron.schedule('0 7,11,15,19 * * *', async () => {
-    console.log('running a task count', i++);
-    await mainFunction()
-
+let i = 1;
+cron.schedule("0 7,11,15,19", async () => {
+  console.log("running a task count", i++);
+  await mainFunction();
 });
 
 const mainFunction = async () => {
-    try {
-        await syncTwiiterToken()
-        const pageData = await getfirebaseDatabase();
-        const responseImage = await postImageGenerate(pageData);
-        const responseCaption = await postCaptionsGenerate(pageData);
-        const originalCaption = responseCaption.replace(/[^\w\s]/gi, "");
-        const data = { ...pageData, caption: originalCaption, img: responseImage }
-        await postImageTwitter(data);
-        await postImageFacebook(data)
-        await postImageInstagram(data)
-        console.log('Done ✅')
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await syncTwiiterToken();
+    const pageData = await getfirebaseDatabase();
+    const responseImage = await postImageGenerate(pageData);
+    const responseCaption = await postCaptionsGenerate(pageData);
+    const originalCaption = responseCaption.replace(/[^\w\s]/gi, "");
+    const data = { ...pageData, caption: originalCaption, img: responseImage };
+    await postImageTwitter(data);
+    await postImageFacebook(data);
+    await postImageInstagram(data);
+    console.log("Done ✅");
+  } catch (error) {
+    console.log(error);
+  }
+};
 //    0 7,11,15,19 * * *    => 7am, 11pm, 3pm, 7pm
 //   */5 * * * * *  => 5 sec
 //   * * * * *  => every minutes
